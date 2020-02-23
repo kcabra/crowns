bf41b29f1203080ef127a93714db925251f6ebe5
kcabra <kcabrak@gmail.com>
2020-02-23 19:31:27 -0300

    Add tile_tools script
    Improved usability of tilemap editor by making changes to tileset
    resource with tile_tools.gd, also tweaked tilesheet and expanded map.

---
+++ map/tile_tools.gd
@@ -0,0 +1,26 @@
+tool
+extends EditorScript
+
+func _run():
+    var ts : TileSet = load("res://map/tileset.tres")
+    var nd = get_tile_names(ts)
+    for i in nd.keys():
+        nd[i] = change_name_suffix(nd[i])
+        ts.tile_set_name(i, nd[i])
+
+func get_tile_names(ts : TileSet):
+    var ids = ts.get_tiles_ids()
+    var names = Dictionary()
+    for id in ids:
+        var name = ts.tile_get_name(id)
+        names[id] = name
+#        print(String(id)+": "+name)
+    return names
+
+func change_name_suffix(name : String):
+    var digit
+    for i in range(10):
+        var d = String(i)
+        if name.ends_with(" "+d):
+            return name.trim_suffix(" "+d)+" 0"+d
+    return name
